71d187ca5bed12ec30f1d74a1e71a1d00324adc3
Vasco Alves <vascoalves8@gmail.com>
2020-01-03 22:45:04 +0000

    página informação atividade update

--- src/pages/DescricaoAtividade.tsx
+++ src/pages/DescricaoAtividade.tsx
@@ -13,6 +13,44 @@ const Atividade: React.FC<UserDetailPageProps> = ({match}) => {
   const [atividade, setAtividade ] = useState({atividade: "", categoria: {}, categoria_id: null, descricao: "", duracao: "", hora: "", id: null, local: "", moderador: null});
   const [ txtTitulo, setTxtTitulo ] = useState("A carregar atividade...");
 
+  const styl_infoHorario = {
+    display: "flex",  
+    alignItems: "center", 
+    justifyContent: "space-between",
+    padding: "8px 10px",
+    color: "#4a4a4a"
+  }
+
+  const styl_infoHorario_dentro = {
+    display: "flex",  
+    alignItems: "center", 
+    justifyContent: "flex-start",
+    flexGrow: 1
+  }
+
+  const stly_hora = {
+    background: "#9b9b9b",
+    padding: "2px 15px",
+    borderRadius: "15px",
+    display: "flex",
+    justifyContent: "center",
+    color: "white",
+    fontSize: "14px"
+  }
+
+  const styl_areaDescricao = {
+    textAlign: "justify" as "justify",
+    padding: "2px 15px",
+    color: "#4a4a4a"
+  }
+
+  const styl_btnAdicionar = {
+    color: "#A61526",
+    background: "#FFEC00",
+    backgroundColor: "#FFEC00",
+    marginTop: "50px"
+  }
+
   useIonViewWillEnter(() => {
     // -- obter lista de categorias
     axios({
@@ -57,9 +95,26 @@ const Atividade: React.FC<UserDetailPageProps> = ({match}) => {
           <div className="areaInformacao" style={{padding: "8px 16px", marginTop: "10px"}}>
             <div>
               <IonText style={{fontSize: "26px", color: "#4a4a4a", fontWeight: "bold", marginBottom: 0, paddingBottom: 0, lineHeight: "0px"}}><p>{atividade.atividade}</p></IonText>
-              <IonText style={{fontSize: "12px", color: "#757575", marginTop: 0, paddingTop: 0}}><p>{atividade.atividade}</p></IonText>
+              <IonText style={{fontSize: "12px", color: "#757575", marginTop: 0, paddingTop: 0}}><p>Moderador: {atividade.atividade}</p></IonText>
+              <div style={styl_infoHorario}>
+                <div style={styl_infoHorario_dentro}>
+                  <p style={{marginRight: "5px"}}>Início: </p>
+                  <div style={stly_hora}>{(atividade.hora as string).slice(0, -3)}</div>
+                </div>
+                
+                <div style={styl_infoHorario_dentro}>
+                  <p style={{marginRight: "5px"}}>Duração: </p>
+                  <div style={stly_hora}>{(atividade.duracao as string).slice(0, -3)}</div>
+                </div>
+              </div>
+            </div>
+            <div style={styl_areaDescricao}>
+              <p>{atividade.descricao}</p>
             </div>
 
+            <div className="ion-margin btnAmarelo">
+                <IonButton type="button" style={styl_btnAdicionar}  onClick={() => {}} size="large" expand="block">PARTICIPAR</IonButton>
+            </div>
           </div>
         }
       </IonContent>
