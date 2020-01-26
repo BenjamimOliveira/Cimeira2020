c6b2b177d518cbbcb9d9fa05c98cf6e6a9296289
BenjamimOliveira <bmoliveira03@gmail.com>
2020-01-26 16:13:09 +0000

    Alteração de 'history.push()' para 'history.replace()' quando o login é efetuado, de forma a impedir que se volte ao ecrã de login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -52,7 +52,7 @@ const Login: React.FC = () => {
             if(resultado.data.cod === 6){
                 setToast({state: true, message: "Login realizado com sucesso!"});
                 localStorage.setItem("UtilizadorLogin", email);
-                history.push("/home");
+                history.replace("/home");
             } else if(resultado.data.cod === 4){
                 setToast({state: true, message: "Ainda é necessário validar esta conta! Obtenha a chave de validação no seu email."});
                 localStorage.setItem("email_validar_conta", email);
