bedf345a71c2fd2da7797c08a1fefa4736e2bff1
BenjamimOliveira <bmoliveira03@gmail.com>
2020-01-26 18:15:12 +0000

    Page 'Workshops' -> Template corrigido, icon de partilha retirado da toolbar

--- src/components/WorkshopList.tsx
+++ src/components/WorkshopList.tsx
@@ -10,26 +10,38 @@ interface Data {
   }
 
 const WorkshopList: React.FC<Data> = (props) => {
+
+    
+    const rect = {
+        minWidth: "50px",
+        margin: "0 auto"
+    };
+    const stly_hora = {
+        margin: "0 auto",
+        background: "#9b9b9b",
+        padding: "2px",
+        borderRadius: "15px",
+        display: "flex",
+        justifyContent: "center",
+        color: "white",
+        fontSize: "14px"
+    };
+
     return (
-        <IonGrid class="itemFora">
-            <IonRow class="row">
-                <IonCol size="2" class="hora ion-text-center">                   
-                    <IonText class="ion-text-center"><div>{props.hora}</div></IonText>             
-                </IonCol>
-
-                <IonCol size="8" class="desc">                   
-                    <IonText>{props.desc}</IonText>                
-                </IonCol>
-
-                <IonCol size="2">
-                    <IonItem>
-                        <Link to={"/descrWorkshop/" + props.id}>
-                            <IonIcon name="arrow-forward" style={{ color: "#4b4b4b", fontSize: "24px", minWidth: "24px" }} ></IonIcon>
-                        </Link>
-                    </IonItem>
-                </IonCol>
-            </IonRow>                      
-        </IonGrid> 
+        <div style={{ display: "flex", padding: "0 10px", alignItems: "center", justifyContent: "space-between" }}>
+        <div style={rect}>
+            <div style={stly_hora}>
+                {props.hora}
+            </div>
+        </div>
+        <div style={{ marginLeft: "15px", width: "100%", display: "flex", alignItems: "center", justifyContent: "space-between" }} >
+            <p style={{ marginRight: "auto", color: "#4b4b4b" }}>{props.desc.toUpperCase()}</p>
+            <Link to={"/descrWorkshop/" + props.id}>
+                <IonIcon name="arrow-forward" style={{ color: "#4b4b4b", fontSize: "24px", minWidth: "24px" }} ></IonIcon>
+            </Link>
+        </div>
+    </div>
+        
     );
 };
 
